4fdb85de575a7bb53af8f846b3ce19d2d879d76b
Martin Zikmund <martin@zikmund.dev>
2021-04-14 21:46:18 +0200

    Basic solution

---
+++ TeslaUiSample/TeslaUiSample.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace TeslaUiSample.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new TeslaUiSample.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ TeslaUiSample/TeslaUiSample.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace TeslaUiSample.Skia.Gtk
+{
+}
